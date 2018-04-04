bd67df6e704cd4511146817390a2e25afc767ae7
Evgen Anufriev <eanufriev@aginity.com>
2018-04-05 00:00:24 +0300

    chc

--- containers/postgres/scripts/100-ambari.sh
+++ containers/postgres/scripts/100-ambari.sh
@@ -9,21 +9,21 @@ wget -nv ${AMBARI_DDL_URL} -O Ambari-DDL-Postgres-CREATE.sql
 
 cat Ambari-DDL-Postgres-CREATE.sql
 
-# #sed -i "s/\${ambariSchemaVersion}/2.5.2/g" Ambari-DDL-Postgres-CREATE.sql
+#sed -i "s/\${ambariSchemaVersion}/2.5.2/g" Ambari-DDL-Postgres-CREATE.sql
 
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-#     create database ambari;
-#     create user ambari with password 'dev';
-#     GRANT ALL PRIVILEGES ON DATABASE ambari TO ambari;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    create database ambari;
+    create user ambari with password 'dev';
+    GRANT ALL PRIVILEGES ON DATABASE ambari TO ambari;
 
-#     CREATE SCHEMA ambari AUTHORIZATION ambari;
-#     ALTER SCHEMA ambari OWNER TO ambari;
-#     ALTER SCHEMA public OWNER to ambari;
+    CREATE SCHEMA ambari AUTHORIZATION ambari;
+    ALTER SCHEMA ambari OWNER TO ambari;
+    ALTER SCHEMA public OWNER to ambari;
 
-#     ALTER ROLE ambari SET search_path to 'ambari', 'public';
+    ALTER ROLE ambari SET search_path to 'ambari', 'public';
 
-#     \connect ambari ambari;
-#     \i Ambari-DDL-Postgres-CREATE.sql 
-# EOSQL
+    \connect ambari ambari;
+    \i Ambari-DDL-Postgres-CREATE.sql 
+EOSQL
 
-# cp /pg_hba.conf /var/lib/postgresql/data/
+cp /pg_hba.conf /var/lib/postgresql/data/
