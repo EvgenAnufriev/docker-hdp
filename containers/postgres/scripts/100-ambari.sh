3b177103aaa9976fa6d8274cac0110f057548d82
Evgen Anufriev <eanufriev@aginity.com>
2018-04-04 23:00:26 +0300

    ch

--- containers/postgres/scripts/100-ambari.sh
+++ containers/postgres/scripts/100-ambari.sh
@@ -10,24 +10,22 @@ cd ~/
 #wget https://raw.githubusercontent.com/apache/ambari/branch-2.5/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
 echo AMBARI_DDL_URL=$AMBARI_DDL_URL
 
-exit
+# wget ${AMBARI_DDL_URL}
+# #sed -i "s/\${ambariSchemaVersion}/2.5.2/g" Ambari-DDL-Postgres-CREATE.sql
 
-wget ${AMBARI_DDL_URL}
-#sed -i "s/\${ambariSchemaVersion}/2.5.2/g" Ambari-DDL-Postgres-CREATE.sql
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+#     create database ambari;
+#     create user ambari with password 'dev';
+#     GRANT ALL PRIVILEGES ON DATABASE ambari TO ambari;
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create database ambari;
-    create user ambari with password 'dev';
-    GRANT ALL PRIVILEGES ON DATABASE ambari TO ambari;
+#     CREATE SCHEMA ambari AUTHORIZATION ambari;
+#     ALTER SCHEMA ambari OWNER TO ambari;
+#     ALTER SCHEMA public OWNER to ambari;
 
-    CREATE SCHEMA ambari AUTHORIZATION ambari;
-    ALTER SCHEMA ambari OWNER TO ambari;
-    ALTER SCHEMA public OWNER to ambari;
+#     ALTER ROLE ambari SET search_path to 'ambari', 'public';
 
-    ALTER ROLE ambari SET search_path to 'ambari', 'public';
+#     \connect ambari ambari;
+#     \i Ambari-DDL-Postgres-CREATE.sql 
+# EOSQL
 
-    \connect ambari ambari;
-    \i Ambari-DDL-Postgres-CREATE.sql 
-EOSQL
-
-cp /pg_hba.conf /var/lib/postgresql/data/
+# cp /pg_hba.conf /var/lib/postgresql/data/
