6f25ba3af44c18b157db0a04db7c4ab434558481
Evgen Anufriev <eanufriev@aginity.com>
2018-04-09 22:34:44 +0300

    ch

--- start_hdp_cluster.sh
+++ start_hdp_cluster.sh
@@ -2,4 +2,7 @@
 
 source ./env/set_env.sh
 
+HDP_SINGLE_NODE_COMPOSE_FILE=compose/single-container.yml
+HDP_REPO_NODE_COMPOSE_FILE=compose/repo-cache.yml
+
 docker-compose -f $HDP_SINGLE_NODE_COMPOSE_FILE up --detach --remove-orphans
