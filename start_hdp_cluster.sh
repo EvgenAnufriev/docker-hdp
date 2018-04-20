60d13160fa9372d2a01b96a4c732567f4e2a8365
Evgen Anufriev <eanufriev@aginity.com>
2018-04-20 12:26:14 +0300

    ch

--- start_hdp_cluster.sh
+++ start_hdp_cluster.sh
@@ -8,6 +8,9 @@ export HOST_GROUPS=hostgroups/multi-container.json
 
 docker-compose -f $HDP_COMPOSE_FILE up --detach --remove-orphans
 
+sleep 60
+
+
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/HDP --data-binary "@$BLUEPRINT"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_STACK/operating_systems/$OS_VER/repositories/HDP-${HDP_STACK} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev --data-binary "@$HOST_GROUPS"
