566705e692cae447c35cc35160baaa3776eba8bd
Evgen Anufriev <eanufriev@aginity.com>
2018-04-20 13:31:55 +0300

    ch

--- start_hdp_cluster.sh
+++ start_hdp_cluster.sh
@@ -15,7 +15,7 @@ curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/bl
 echo BLUEPRINT DONE
 
 echo BASEURL=$BASEURL
-curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_STACK/operating_systems/$OS_VER/repositories/HDP-${HDP_STACK} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
+curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_STACK/operating_systems/$OS_VER/repositories/HDP-${HDP_STACK} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":false}}'
 echo BASEURL DONE
 
 echo HOST_GROUPS=$HOST_GROUPS
