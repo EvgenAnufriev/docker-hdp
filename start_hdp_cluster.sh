889f12c23bae3277f12b1393c4fb8b57a8729f0f
Evgen Anufriev <eanufriev@aginity.com>
2018-04-23 12:14:35 +0300

    chi

--- start_hdp_cluster.sh
+++ start_hdp_cluster.sh
@@ -2,14 +2,14 @@
 
 source ./env/set_env.sh
 
-export BASE_URL=http://$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-${HDP_VER}
-export BASE_UTILS_URL=$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-UTILS-${HDP_UTILS_VER}
+export BASE_URL_REPO=http://$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-${HDP_VER}
+export BASE_UTILS_URL_REPO=$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-UTILS-${HDP_UTILS_VER}
 
 export BLUEPRINT=blueprints/HDP.json
 export HOST_GROUPS=hostgroups/multi-container.json
 
-export BASEURL_JSON='{"Repositories":{"repo_name": "'${HDP_VER}'", "base_url":"'$BASE_URL'", "verify_base_url":true}}'
-export BASEURL_UTILS_JSON='{"Repositories":{"repo_name": "'${HDP_VER}'", "base_url":"'$BASE_UTILS_URL'", "verify_base_url":true}}'
+export BASEURL_JSON='{"Repositories":{"repo_name": "'${HDP_VER}'", "base_url":"'$BASE_URL_REPO'", "verify_base_url":true}}'
+export BASEURL_UTILS_JSON='{"Repositories":{"repo_name": "'${HDP_VER}'", "base_url":"'$BASE_UTILS_URL_REPO'", "verify_base_url":true}}'
 
 docker-compose -f $HDP_COMPOSE_FILE up --detach --remove-orphans
 
@@ -19,11 +19,11 @@ echo BLUEPRINT =$BLUEPRINT
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/HDP --data-binary "@$BLUEPRINT"
 echo BLUEPRINT DONE
 
-echo BASE_URL=$BASE_URL
-echo BASEURL_UTILS_JSON=$BASEURL_UTILS_JSON
+echo BASE_URL_REPO=$BASE_URL_REPO
+echo BASE_UTILS_URL_REPO=$BASE_UTILS_URL_REPO
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_STACK/operating_systems/redhat$OS_VER/repositories/HDP-${HDP_STACK} -d $BASEURL_JSON
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_STACK/operating_systems/redhat$OS_VER/repositories/HDP-${HDP_UTILS_VER} -d $BASEURL_UTILS_JSON
-echo BASEURL DONE
+echo BASE_URL_REPO DONE
 
 
 echo HOST_GROUPS=$HOST_GROUPS
