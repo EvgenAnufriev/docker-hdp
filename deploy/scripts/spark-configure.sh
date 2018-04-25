c49c12987f3e34a76db04681e44624cfabc72f5d
Evgen Anufriev <eanufriev@aginity.com>
2018-04-25 14:09:01 +0300

    ch

--- deploy/scripts/spark-configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copy modified spark configuration file to $SPARK_HOME/conf
-sudo cp /tmp/files/fairscheduler.xml /etc/spark/conf/
-
-# Start Spark thriftserver
-sudo /usr/lib/spark/sbin/start-thriftserver.sh --master yarn
