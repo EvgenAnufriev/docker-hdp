ae0b913318eab8f19317f6ab35c447ae2a68c1d5
Evgen Anufriev <eanufriev@aginity.com>
2018-04-26 13:55:52 +0300

    ch

--- start_hdp_cluster.sh
+++ start_hdp_cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 source ./env/set_env.sh
+
+set -x
 
 export BASE_URL_REPO=http://$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-${HDP_VER}
 export BASE_UTILS_URL_REPO=http://$REPO_HOST_ADDRESS/hdp/${OS_VER}/HDP-UTILS-${HDP_UTILS_VER}
